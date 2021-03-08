6dcb9d483d6decc77a0ad450c09fe25bf906b966
Qingyu Chen <qingyuc@uw.edu>
2021-03-07 21:08:55 -0800

    fix style issues with FLAKE8, disable PYLINT for now

--- yelp_reviews/code/data_collection/api_data.py
+++ yelp_reviews/code/data_collection/api_data.py
@@ -2,7 +2,6 @@ import requests
 import json
 import time
 import pandas as pd
-import csv
 
 
 def yelp_search(api_key, params):
@@ -14,17 +13,13 @@ def yelp_search(api_key, params):
         location: location keywords (Seattle, etc.)
     Returns JSON
     """
-
     search_url = "https://api.yelp.com/v3/businesses/search"
 
     headers = {
-        "Authorization" : "Bearer %s" % api_key
+        "Authorization": "Bearer %s" % api_key
     }
-
     response = requests.get(search_url, params = params, headers = headers)
-
     data = json.loads(response.text)
-
     return data
 
 
@@ -40,45 +35,39 @@ def all_restaurants(api_key, params):
     """
 
     data1 = yelp_search(api_key, params)
-
     records_num  = data1["total"]
     requests_num = records_num // 20 + 1
     offset = 0
     result = []
 
     for i in range(requests_num):
-        #20 restaurants per request
+        # 20 restaurants per request
         curr_offset = offset + i * 20
         params["offset"] = curr_offset
         data = yelp_search(api_key, params)
         result += data["businesses"]
-        #pause slightly between requests
+        # Pause slightly between requests
         time.sleep(.300)
 
     return result
 
 def parse_api_response(api_response):
-
     """
     Parse the API response into a Pandas DataFrame
     API response is all of the restaurants matched from yelp_scraping
     """
-
     df = pd.DataFrame(api_response)
     category_list = []
-
     for i in range(len(df)):
         cat_i = [cat["alias"] for cat in df["categories"][i]]
         category_list.append(",".join(cat_i))
-
     latitude = [coord.get("latitude") for coord in df["coordinates"]]
     longitude = [coord.get("longitude") for coord in df["coordinates"]]
-
     df["category"] = category_list
     df["latitude"] = latitude
     df["longitude"] = longitude
-
-    df_return = df.drop(columns = ["coordinates", "image_url", "is_closed", "categories", "location", "display_phone", "distance"])
+    df_return = df.drop(columns = ["coordinates", "image_url", "is_closed", "categories", 
+                                    "location", "display_phone", "distance"])
 
     return df_return
 
@@ -87,10 +76,10 @@ def write_api_data():
     api_key = 'Y0vpAcCzpLY3l5VSChBzAcRpy-JrWmmaOenf'\
                     'Uf-AGrC4lKtc79YDH503ZZSURFVGsAx_I1-Xo'\
                     '0T6YykBPmaOalvnGubVhpIH_K0kfIcWEh0FLftyNyUQ75MXaW0wYHYx'
-    tacos = all_restaurants(api_key, params={"term":"taco",
+    tacos = all_restaurants(api_key, params={"term":"taco", 
                 "location":"University District,Seattle", 
                 "categories": "restaurants"})
     taco_restaurants_df = parse_api_response(tacos)
     taco_restaurants_df.to_csv('api_data.csv')
 
-#write_api_data()
+# write_api_data()
--- yelp_reviews/code/tests/__init__.py
+++ yelp_reviews/code/tests/__init__.py
@@ -1,2 +1,6 @@
+'''
+tests init
+'''
+
 from yelp_reviews.code.data_collection.api_data import *
 from yelp_reviews.code.data_collection.reviews_scraper import *
