26eea7f10d4a5743e1a6418a9dc3509d659b5059
sindhumadhadi09 <79022549+sindhumadhadi09@users.noreply.github.com>
2021-03-08 14:36:58 -0800

    Add files via upload

--- test_yelp_api_data.py
+++ test_yelp_api_data.py
@@ -12,39 +12,46 @@ params={"term":"taco",
                     "categories": "restaurants"}
 class MyTestCase(unittest.TestCase):
     def test_api_response(self):
-
+        """
+        TEST FOR API RESPONSE
+        To check if the data in json frame is parsed into a panda dataframe correctly.
+        """
         tacos = all_restaurants(api_key,params)
         taco_restaurants_df = parse_api_response(tacos)
-        #Test the dataframe specs
         self.assertEqual(len(tacos), len(taco_restaurants_df))
         self.assertEqual(taco_restaurants_df.shape, (156,12))
 
-        #Test for a single row
+        """
+            TEST FOR A SINGLE ROW:
+            Using a particular restaurant url to check if all the rows in the dataframe are correct:
+            Scope: If we want to check for any other cases , need to change the url as test data needed is hardcoded.
+        """
 
-        restaurant_url_list = taco_restaurants_df['url'].tolist()
+        url_name ='https://www.yelp.com/biz/tnt-taqueria-seattle?adjust_creative=Yd84IPqpgzteXDQ2QE83uA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Yd84IPqpgzteXDQ2QE83uA'
+        TNT_Taqueria=taco_restaurants_df.loc[taco_restaurants_df['url'] == url_name]
 
-        for i in range(len(restaurant_url_list)):
-            url = restaurant_url_list[i]
-            url_name='https://www.yelp.com/biz/off-the-rez-seattle?adjust_creative=Yd84IPqpgzteXDQ2QE83uA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Yd84IPqpgzteXDQ2QE83uA'
-            if (url==url_name):
-                Off_the_Rez = taco_restaurants_df.iloc[i]
-                self.assertEqual(Off_the_Rez['name'], 'Off the Rez')
-                self.assertEqual(Off_the_Rez['price'], '$')
-                self.assertEqual(Off_the_Rez['rating'], 4.0)
-                self.assertEqual(Off_the_Rez['category'], 'foodtrucks,burgers,tacos')
+        self.assertEqual(TNT_Taqueria.iloc[0]['name'],'TNT Taqueria')
+        self.assertEqual(TNT_Taqueria.iloc[0]['price'], '$')
+        self.assertEqual(TNT_Taqueria.iloc[0]['rating'], 4.0)
+        self.assertEqual(TNT_Taqueria.iloc[0]['category'], 'mexican')
 
 
 
-    def test_parse_page(self):
+    def test_scrape_page(self):
+        """
+           TEST SCRAPING FUNCTIONS:
+           To check if the the numbers of reviews extracted  in a page are 20
+        """
 
         tacos = all_restaurants(api_key, params)
         taco_restaurants_df = parse_api_response(tacos)
 
-        #to check page_parsing
+        #To check page_parsing
         test_url = taco_restaurants_df.loc[1]['url']
         test_reviews=extract_reviews(test_url)
         self.assertEqual(len(test_reviews),20)
-        #parse Instance
+
+        #Parse Instance
         self.assertIsInstance(parse_page('tests/test.html'), list, "is list")
 
 
