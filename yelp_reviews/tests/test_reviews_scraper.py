a36dbbb5c0aa9b70c0f8a929c59fc84bae762367
Qingyu Chen <qingyuc@uw.edu>
2021-03-11 17:10:39 -0800

    fixed test_review_scraper.py

--- yelp_reviews/tests/test_reviews_scraper.py
+++ yelp_reviews/tests/test_reviews_scraper.py
@@ -15,8 +15,9 @@ from yelp_reviews.data_collection.reviews_scraper import (
 )
 
 DIR_PATH = str(Path(os.getcwd()))
-API_KEY = Path(os.path.join(DIR_PATH, "yelp_reviews/tests/data", "api_key.txt")).read_text()
-PARAM_PATH = Path(os.path.join(DIR_PATH, "yelp_reviews/tests/data", "params.txt"))
+DATA_FOLDER = "yelp_reviews/tests/data"
+API_KEY = Path(os.path.join(DIR_PATH, DATA_FOLDER, "api_key.txt")).read_text()
+URL_PATH = Path(os.path.join(DIR_PATH, DATA_FOLDER, "url.txt"))
 HTML_PATH = Path(os.path.join(DIR_PATH, "yelp_reviews/tests/data", "test1.html"))
 
 class WebScraperTestCase(unittest.TestCase):
@@ -25,19 +26,10 @@ class WebScraperTestCase(unittest.TestCase):
            TEST SCRAPING FUNCTIONS:
            To check if the the numbers of reviews extracted  in a page are 20
         """
-        params_list=[]
-        with open(PARAM_PATH,'r') as file:
-            for line in file:
-                params_list.append(line.strip())
-                
-        params={"term":params_list[0],"location":params_list[1],
-                "categories":params_list[2]}
-
-        tacos = all_restaurants(API_KEY, params)
-        taco_restaurants_df = parse_api_response(tacos)
-
         # Retrieve HTML Test Case
-        test_url = taco_restaurants_df.loc[1]['url']
+        with open(URL_PATH, 'r') as file:
+            for line in file:
+                test_url = line
         (status_code, html_obj) = retrieve_html(test_url)
         self.assertEqual(status_code, 200)
 
