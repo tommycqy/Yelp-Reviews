d51121ea30975c60253d6a531daeebb13f07e45e
Qingyu Chen <qingyuc@uw.edu>
2021-03-15 19:28:52 -0700

    complete setup.py and added comments for scraper-demo

--- setup.py
+++ setup.py
@@ -2,40 +2,30 @@ from setuptools import setup, find_packages
 
 PACKAGES = find_packages()
 
-CLASSIFIERS = ["Development Status :: 3 - Alpha",
-               "Environment :: Console",
+CLASSIFIERS = ["Environment :: Console",
                "Intended Audience :: Science/Research",
-               "License :: OSI Approved :: MIT License",
                "Operating System :: OS Independent",
                "Programming Language :: Python",
-               "Topic :: Scientific/Engineering"]
-
+               "Topic :: Yelp Reviews"]
 
 NAME = "Yelp Reviews"
 DESCRIPTION = "A data science project using Yelp Fusion API and Web Scraping"
-URL = ""
-DOWNLOAD_URL = ""
-LICENSE = ""
-AUTHOR = "Tommy Chen | Emily | Sindhu"
-AUTHOR_EMAIL = ""
-PLATFORMS = ""
+URL = "https://github.com/tommycqy/Yelp-Reviews"
+AUTHOR = "Tommy Chen | Emily Yamauchi| Sindhu Madhadi"
+AUTHOR_EMAIL = "qingyuc@uw.edu, eyamauch@uw.edu, sindhu09@uw.edu"
+PLATFORMS = "Linux, Windows and MacOS"
 VERSION = "__version__ 1.0"
-PACKAGE_DATA = {}
-REQUIRES = ["numpy", "pandas", "requests", "bs4", "nltk", "mapboxgl"]
 
 opts = dict(name=NAME,
             description=DESCRIPTION,
             url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
             classifiers=CLASSIFIERS,
             author=AUTHOR,
             author_email=AUTHOR_EMAIL,
             platforms=PLATFORMS,
             version=VERSION,
-            packages=PACKAGES,
-            install_requires=REQUIRES,
-            requires=REQUIRES)
+            packages=PACKAGES)
+
 
 if __name__ == '__main__':
     setup(**opts)
